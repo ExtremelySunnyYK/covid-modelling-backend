3d7c51c0de77b16da263184aecf1de7c360a752e
ExtremelySunnyYk <chiayongkang@hotmail.com>
2021-11-20 23:12:31 +0800

    get request working

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ model = Model()
 
 @app.route('/')
 def home():
-    return 'Hello, World!'
+    return render_template('index.html')
 
 
 @ app.route('/predict', methods=['GET'])
@@ -29,6 +29,16 @@ def predict():
         # convert numpy array to list
         prediction = prediction.tolist()
         return jsonify(prediction)
+
+    elif request.method == 'POST':
+        data = request.get_json()
+        app.logger.info(data)
+        lst = json.loads(data)
+        prediction = model.predict(lst)
+        # convert numpy array to list
+        prediction = prediction.tolist()
+        return jsonify(prediction)
+
 
 # @ app.route('/view', methods=['POST', 'GET'])
 # def view():
